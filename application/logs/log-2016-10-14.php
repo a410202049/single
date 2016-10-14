fe0e3c167618f5790339fb6a48247fee6d958256
kerrygao <1509699669@qq.com>
2016-10-14 11:21:02 +0800

    ad

--- application/logs/log-2016-10-14.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-10-14 10:12:15 --> Could not find the language line "today"
-ERROR - 2016-10-14 10:12:38 --> Severity: error --> Exception: Failed to connect to accounts.google.com port 443: Timed out D:\phpStudy\WWW\single\application\third_party\google-api-php-client\src\Google\IO\Curl.php 126
-ERROR - 2016-10-14 10:12:46 --> 404 Page Not Found: Upload/article
